20eb9bf994b87b839634a7765049474755e8abc4
Bruno Bravo <bruno.bravo@sprint.com>
2019-07-22 11:44:38 -0300

    Adding JUnit dependency and test folder

---
+++ src/test/java/com/mobiquityinc/packer/PackerTest.java
@@ -0,0 +1,12 @@
+package com.mobiquityinc.packer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PackerTest {
+
+    @Test
+    void pack() {
+    }
+}
